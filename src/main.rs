1dc16f60f70c2b01d472906b0c122939531d0c53
dolphin2410 <dolphin2410@outlook.kr>
2022-03-15 17:20:19 +0900

    Fix bug

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@ use pcap_parser::*;
 use pcap_parser::data::PacketData;
 use pcap_parser::traits::PcapReaderIterator;
 use regex::Regex;
+use winapi::um::winuser::{SW_NORMAL, SW_SHOWDEFAULT};
+use std::ffi::CString;
 use std::fs::File;
 use std::process::{Command, Stdio};
 use std::io;
 use std::io::prelude::*;
+use clap::Parser;
 
-#[macro_use]
-extern crate dotenv_codegen;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    nocapture: bool,
+
+    #[clap(short, long, default_value_t = String::from("capture.pcapng"))]
+    file: String
+}
 
 fn pause() {
     let mut stdin = io::stdin();
@@ -20,8 +30,8 @@ fn pause() {
     let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
-fn main() {
-    let ip = dotenv!("IP_ADDRESS"); // Why this error? But it's not an error
+fn capture_packet(default: &str) {
+    let ip = "115.68.13.56"; 
     Command::new("pktmon").args(vec!["filter", "remove"]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
     Command::new("pktmon").args(vec!["filter", "add", "-i", ip, "-t", "tcp", "-p", "80"]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
     Command::new("pktmon").args(vec!["filter", "add", "-i", ip, "-t", "tcp", "-p", "443"]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
@@ -29,18 +39,63 @@ fn main() {
 
     pause();
     Command::new("pktmon").args(vec!["stop"]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
-    Command::new("pktmon").args(vec!["etl2pcap", "PktMon.etl", "-o", "capture.pcapng"]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
+    Command::new("pktmon").args(vec!["etl2pcap", "PktMon.etl", "-o", default]).stdout(Stdio::inherit()).spawn().unwrap().wait().unwrap();
+}
 
+fn elevate() {
+    println!("Reached");
+    use winapi::um::shellapi::ShellExecuteA;
+    use winapi::um::wincon::GetConsoleWindow;
+    let runas = CString::new("runas").unwrap();
+    let program = CString::new(std::env::current_exe().unwrap().as_os_str().to_str().unwrap()).unwrap();
+    let args = CString::new(std::env::args().collect::<Vec<String>>()[1..].join(" ")).unwrap();
 
-    let file = File::open("capture.pcapng").unwrap();
+    println!("{}", args.to_str().unwrap());
+
+    unsafe {
+        ShellExecuteA(
+            GetConsoleWindow(), 
+            runas.as_ptr(), 
+            program.as_ptr(), 
+            args.as_ptr(), 
+            std::ptr::null_mut(), 
+            SW_NORMAL);
+    };
+}
+
+fn main() {
+    use device_query::{DeviceQuery, DeviceState, Keycode};
+    use is_elevated::is_elevated;
+
+    if !is_elevated() {
+        elevate();
+        return;
+    }
+
+    let args = Args::parse();
+
+    if !args.nocapture {
+        capture_packet(args.file.as_str());
+    }
+
+    let file = File::open(args.file.as_str()).unwrap();
     let input = read(file);
 
     println!("{:?}", &input);
+
+    let device_state = DeviceState::new();
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+        if keys.iter().any(|key| { key == &Keycode::LControl }) && keys.iter().any(|key| { key == &Keycode::Q }) {
+            tell(&input);
+            break;
+        }
+    }
 }
 
 fn parse_http(value: String) -> Result<Vec<u8>, &'static str> {
-    let param_start = dotenv!("PARAM_START");
-    let method_start = dotenv!("METHOD_START");
+    let param_start = "mem_seq";
+    let method_start = "POST /Player/StudyResultSave";
     if value.starts_with(param_start) {
         return match parse_data(value) {
             Ok(data) => Ok(data),
@@ -61,16 +116,12 @@ fn parse_http(value: String) -> Result<Vec<u8>, &'static str> {
 }
 
 fn parse_data(value: String) -> Result<Vec<u8>, &'static str> {
-    let data_target = dotenv!("DATA_TARGET");
-    let split_a = dotenv!("SPLIT_A");
-    let split_b = dotenv!("SPLIT_B");
-    let split_c = dotenv!("SPLIT_C");
     for component in value.split("&") {
-        if component.starts_with(data_target) {
-            let raw_value = component.replace(&format!("{}=", data_target), "");
-            let answers = raw_value.split(split_a);
+        if component.starts_with("itemsAnswer") {
+            let raw_value = component.replace("itemsAnswer=", "");
+            let answers = raw_value.split("%40%23%400%40%2F%40");
             let vec = answers.map(|data| {
-                data.replace(split_b, "").replace(split_c, "").parse::<u8>().unwrap()
+                data.replace("0%40%2F%40", "").replace("%40%23%40", "").parse::<u8>().unwrap()
             }).collect::<Vec<u8>>();
             return Ok(vec)
         }
@@ -123,4 +174,35 @@ fn read(file: File) -> Vec<u8> {
         }
     }
     answers
+}
+
+fn tell(answers: &Vec<u8>) {
+    use soloud::*;
+
+    let one = include_bytes!("../assets/one.mp3");
+    let two = include_bytes!("../assets/two.mp3");
+    let three = include_bytes!("../assets/three.mp3");
+    let four = include_bytes!("../assets/four.mp3");
+    let five = include_bytes!("../assets/five.mp3");
+
+    for answer in answers.iter() {
+        let sl = Soloud::default().unwrap();
+        let mut wav = audio::Wav::default();
+        
+        wav.load_mem(match answer {
+            1 => one,
+            2 => two,
+            3 => three,
+            4 => four,
+            5 => five,
+            _ => panic!("Error!")
+        }).unwrap();
+
+        sl.play(&wav);
+        while sl.voice_count() > 0 {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(1500));
+    }
 }
